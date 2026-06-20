5accb364f768878caafcc6018056b2792919ec6c
Joaquin Pozo <joaquin.pozo@usach.cl>
2026-06-20 19:15:36 -0400

    LAB 4 ADD: Escenas de UI para actualizar barra de vida del Slime.

---
+++ scenes/health_bar.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+@onready var health_bar: Sprite2D = $Health
+@onready var default_width = health_bar.region_rect.size.x
+@onready var default_height = health_bar.region_rect.size.y
+
+func update_health(new_health: int) -> void:
+	# Actualiza la vida
+	var new_width = (new_health / 100.0) * default_width
+	health_bar.region_rect = Rect2(0, 0, new_width, default_height)
