8d7949e97bcdeb277b7a0b791ac8cfbc8b21c79e
Joaquin Pozo <joaquin.pozo@usach.cl>
2026-07-04 16:43:13 -0400

    LAB 5 UPD: Agregado mineral_count y signal mineral_collected a AstronautPlayerStats

--- characters/astronaut_player_stats.gd
+++ characters/astronaut_player_stats.gd
@@ -3,6 +3,10 @@ extends Node
 var health: int = 100
 var max_health: int = 100
 var strength: int = 20
+# Variables para llevar el conteo de minerales recolectados
+signal mineral_collected(count: int)
+var mineral_count: int = 0
 
 func reset() -> void:
 	health = max_health
+	mineral_count = 0
